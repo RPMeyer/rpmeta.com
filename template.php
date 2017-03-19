4946b9fa3cca561beb8c855b27fa33425be7669b
Ryan Meyer <Ryan Meyer>
2017-03-19 11:46:30 -0700

    Testing sidebar container modularity

--- template.php
+++ template.php
@@ -30,6 +30,40 @@ class Template
   ga('send', 'pageview');
 </script>
 
+/*
+     * Replace all SVG images with inline SVG
+     */
+     <script>
+        jQuery('img.svg').each(function(){
+            var $img = jQuery(this);
+            var imgID = $img.attr('id');
+            var imgClass = $img.attr('class');
+            var imgURL = $img.attr('src');
+
+            jQuery.get(imgURL, function(data) {
+                // Get the SVG tag, ignore the rest
+                var $svg = jQuery(data).find('svg');
+
+                // Add replaced image's ID to the new SVG
+                if(typeof imgID !== 'undefined') {
+                    $svg = $svg.attr('id', imgID);
+                }
+                // Add replaced image's classes to the new SVG
+                if(typeof imgClass !== 'undefined') {
+                    $svg = $svg.attr('class', imgClass+' replaced-svg');
+                }
+
+                // Remove any invalid XML tags as per http://validator.w3.org
+                $svg = $svg.removeAttr('xmlns:a');
+
+                // Replace image with new SVG
+                $img.replaceWith($svg);
+
+            }, 'xml');
+
+        });
+        </script>
+
 <title>RPMeyer HUB</title>
 </head>
 
@@ -73,10 +107,10 @@ class Template
                 <ul id="socials">
                   <li>
                     <a href="https://www.facebook.com/rpmeta" class="facebook">
-                      <img id='facebookSVG' src="http://rpmeta.com/res/FacebookF.svg" alt=""/></img>
+                      <img id="facebookSVG" class="svg social-media" src="http://rpmeta.com/res/FacebookF.svg" alt=""/></img>
                     </a>
                     <a href="https://twitter.com/arrpmeyer" class="twitter">
-                      <img id='twitterSVG' src="http://rpmeta.com/res/TwitterBird.svg" alt=""/></img>
+                      <img id='twitterSVG' class="svg social-media" src="http://rpmeta.com/res/TwitterBird.svg" alt=""/></img>
                     </a>
                   </li>
                 </ul>
