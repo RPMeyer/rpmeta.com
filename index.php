4119cb6a949db6a4596b71426e70c7e769515543
Ryan Meyer <Ryan Meyer>
2017-03-20 17:38:51 -0700

    Checking Reviews and Products Subdomains

--- index.php
+++ index.php
@@ -12,16 +12,16 @@ $template->drawMainOpen();
 								<h1 class="entry-title">TESTING ENTRY-TITLE (Reviews Products and the Meta)</h1>
 							</header>
 							<section class="entry">
+								<!--Begin new post entry-->
 								<div class="product-splash">
-
+									<a> PRODUCT </a>
 								</div>
-								<p>
+								<!-- <p>
 									<a href="http://www.rpmeta.com/">
-										<img id="splashRPM" class="splash-logo" src="http://rpmeyer.org/RPMeyerPNG2.png"/>
+										<img id="splashRPM" class="splash-logo" src="http://#"/>
 									</a>
-								</p>
+								</p> -->
 								<div class="content-container">
-									<!--Begin new post entry-->
 									<div class='post-1 TESTING' id='post-1'>
 										<h3 class='entry-title'>
 											<a title='TESTING POST-2' rel='bookmark' href='#'>Reviews Products and the Meta</a>
@@ -44,8 +44,73 @@ $template->drawMainOpen();
 										</div>
 									</div>
 								</div>
-
 									<!--END new post entry .hentry-->
+									<!--Begin new post entry-->
+									<div class="product-splash">
+										<a> PRODUCT </a>
+									</div>
+									<!-- <p>
+										<a href="http://www.rpmeta.com/">
+											<img id="splashRPM" class="splash-logo" src="http://#"/>
+										</a>
+									</p> -->
+									<div class="content-container">
+										<div class='post-1 TESTING' id='post-1'>
+											<h3 class='entry-title'>
+												<a title='TESTING POST-2' rel='bookmark' href='#'>Reviews Products and the Meta</a>
+											</h3>
+											<!--BEGIN .entry-content-->
+											<div class='entry-content'>
+												<div class='theExrp'>
+													<a href='http://'>
+														<img src="http://" class="attachment-post-thumbnail" alt="" />
+													</a>
+													<p id='title-of-post'>
+														Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing
+													</p>
+												</div>
+												<hr/>
+												<div>
+													<a class='more-link' href='#'>read more...</a>
+												</div>
+												<!--END .entry-content-->
+											</div>
+										</div>
+									</div>
+										<!--END new post entry .hentry-->
+										<!--Begin new post entry-->
+										<div class="product-splash">
+											<a> PRODUCT </a>
+										</div>
+										<!-- <p>
+											<a href="http://www.rpmeta.com/">
+												<img id="splashRPM" class="splash-logo" src="http://#"/>
+											</a>
+										</p> -->
+										<div class="content-container">
+											<div class='post-1 TESTING' id='post-1'>
+												<h3 class='entry-title'>
+													<a title='TESTING POST-2' rel='bookmark' href='#'>Reviews Products and the Meta</a>
+												</h3>
+												<!--BEGIN .entry-content-->
+												<div class='entry-content'>
+													<div class='theExrp'>
+														<a href='http://'>
+															<img src="http://" class="attachment-post-thumbnail" alt="" />
+														</a>
+														<p id='title-of-post'>
+															Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing
+														</p>
+													</div>
+													<hr/>
+													<div>
+														<a class='more-link' href='#'>read more...</a>
+													</div>
+													<!--END .entry-content-->
+												</div>
+											</div>
+										</div>
+											<!--END new post entry .hentry-->
 
 									<!--Begin new post entry-->
 									<div class='post-2 TESTING' id='post-2'>
