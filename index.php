f828cbd305d7c0bc24044b746fb629514ecf09a7
Ryan Meyer <Ryan Meyer>
2017-04-06 13:04:57 -0700

    Tweaking post headings and images

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $template->drawMainOpen();
 			<!-- <img src="http://rpmeta.com/res/ProductImageLeft.svg" class="product-template"/> -->
 			<a href="#">
 				<div id="product-splash-zelda-test">
-					<img src="http://rpmeta.com/res/EvolutionClimateThumbnail.jpg" alt="" id="BOTWTestingThumb">
+					<img src="http://rpmeta.com/res/EvolutionClimateThumbnail.jpg" alt="" id="SwitchThumb">
 					<img src="http://rpmeta.com/res/ProductTesting.png" alt="" id="ProductTesting">
 				</div>
 			<!-- <img src="http://rpmeta.com/res/ProductImageTest.jpg" class="product-image-left"/> -->
