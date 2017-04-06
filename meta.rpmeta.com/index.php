a03079754fa89f7f543832eefc596300fd7736c6
Ryan Meyer <Ryan Meyer>
2017-04-06 12:51:14 -0700

    Tweaking post headings and images

--- meta.rpmeta.com/index.php
+++ meta.rpmeta.com/index.php
@@ -11,20 +11,17 @@ $template->drawMainOpen();
 	</header>
 	<section class="entry">
 		<!--Begin new post entry-->
-		<div class="product-splash-left">
-			<!-- <img src="http://rpmeta.com/res/ProductImageLeft.svg" class="product-template"/> -->
+		<div class="meta-splash-left">
 			<a href="#">
-				<div id="product-splash-zelda-test">
-					<img src="http://rpmeta.com/res/BoTWTestingThumb.png" alt="" id="BOTWTestingThumb">
+				<div id="meta-splash-placeholder">
+					<img src="http://rpmeta.com/res/EvoClimKillerStrat.jpg" alt="" id="EvoClimKillerStrat">
 					<img src="http://rpmeta.com/res/ProductTesting.png" alt="" id="ProductTesting">
 				</div>
-				<!-- <img src="http://rpmeta.com/res/ProductImageTest.jpg" class="product-image-left"/> -->
 		</div>
-		<!-- <p><a href="http://www.rpmeta.com/"><img id="splashRPM" class="splash-logo" src="http://#"/></a></p> -->
 		<div class="content-container">
 			<div class='post-1 TESTING' id='post-1'>
 				<h3 class='entry-title'>
-					<a title='TESTING POST-2' rel='bookmark' href='#'>Reviews Products and the Meta</a>
+					<a title='TESTING POST-2' rel='bookmark' href='#'>Evolution Climate: Killer Carnivore Strat</a>
 				</h3>
 				<!--BEGIN .entry-content-->
 				<div class='entry-content'>
@@ -33,7 +30,10 @@ $template->drawMainOpen();
 							<img src="http://" class="attachment-post-thumbnail" alt="" />
 						</a>
 						<p id='title-of-post'>
-							Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing
+							Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate
+							Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate
+							Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate
+							Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat
 						</p>
 					</div>
 					<hr/>
@@ -46,14 +46,12 @@ $template->drawMainOpen();
 		</div>
 		<!--END new post entry .hentry-->
 		<!--Begin new post entry-->
-		<div class="product-splash-right">
-			<!-- <img src="http://rpmeta.com/res/ProductImageLeft.svg" class="product-template"/> -->
+		<div class="meta-splash-right">
 			<a href="#">
-				<div id="product-splash-switch-test">
+				<div id="meta-splash-placeholder">
 					<img src="http://rpmeta.com/res/SwitchThumb.png" alt="" id="SwitchThumb">
 					<img src="http://rpmeta.com/res/ProductTestingRight.png" alt="" id="ProductTesting">
 				</div>
-				<!-- <img src="http://rpmeta.com/res/ProductImageTest.jpg" class="product-image-left"/> -->
 		</div>
 		<div class="content-container">
 			<div class='post-1 TESTING' id='post-1'>
