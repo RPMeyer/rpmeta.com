18423ca5276634576936198798f3f68ebf0344d1
Ryan Meyer <Ryan Meyer>
2017-04-06 11:41:32 -0700

    Tweaking post headings and images

--- meta.rpmeta.com/index.php
+++ meta.rpmeta.com/index.php
@@ -1,12 +1,9 @@
 <?php
-
-require_once($_SERVER['DOCUMENT_ROOT'] . '/template.php');
-
-$template = new Template();
-
-$template->drawHeader();
+require_once($_SERVER[ 'DOCUMENT_ROOT'] . '/template.php');
+$template=n ew Template(); $template->drawHeader();
 $template->drawMainOpen();
 ?>
+
 <article class="post_entry_test">
 	<header>
 		<h1 class="entry-title">TESTING ENTRY-TITLE (Reviews Products and the Meta)</h1>
@@ -20,7 +17,7 @@ $template->drawMainOpen();
 					<img src="http://rpmeta.com/res/BoTWTestingThumb.png" alt="" id="BOTWTestingThumb">
 					<img src="http://rpmeta.com/res/ProductTesting.png" alt="" id="ProductTesting">
 				</div>
-			<!-- <img src="http://rpmeta.com/res/ProductImageTest.jpg" class="product-image-left"/> -->
+				<!-- <img src="http://rpmeta.com/res/ProductImageTest.jpg" class="product-image-left"/> -->
 		</div>
 		<!-- <p><a href="http://www.rpmeta.com/"><img id="splashRPM" class="splash-logo" src="http://#"/></a></p> -->
 		<div class="content-container">
@@ -35,8 +32,8 @@ $template->drawMainOpen();
 							<img src="http://" class="attachment-post-thumbnail" alt="" />
 						</a>
 						<p id='title-of-post'>
-													Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing
-												</p>
+							Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing
+						</p>
 					</div>
 					<hr/>
 					<div>
@@ -55,7 +52,7 @@ $template->drawMainOpen();
 					<img src="http://rpmeta.com/res/SwitchThumb.png" alt="" id="SwitchThumb">
 					<img src="http://rpmeta.com/res/ProductTestingRight.png" alt="" id="ProductTesting">
 				</div>
-			<!-- <img src="http://rpmeta.com/res/ProductImageTest.jpg" class="product-image-left"/> -->
+				<!-- <img src="http://rpmeta.com/res/ProductImageTest.jpg" class="product-image-left"/> -->
 		</div>
 		<div class="content-container">
 			<div class='post-1 TESTING' id='post-1'>
@@ -69,8 +66,8 @@ $template->drawMainOpen();
 							<img src="http://" class="attachment-post-thumbnail" alt="" />
 						</a>
 						<p id='title-of-post'>
-														Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing
-													</p>
+							Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing
+						</p>
 					</div>
 					<hr/>
 					<div>
@@ -97,8 +94,8 @@ $template->drawMainOpen();
 							<img src="http://" class="attachment-post-thumbnail" alt="" />
 						</a>
 						<p id='title-of-post'>
-															Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing
-														</p>
+							Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing Nintendo Switch Unboxing
+						</p>
 					</div>
 					<hr/>
 					<div>
@@ -121,8 +118,8 @@ $template->drawMainOpen();
 						<img src="http://" class="attachment-post-thumbnail" alt="" />
 					</a>
 					<p id='title-of-post'>
-													Nintendo Switch Hardware Review
-												</p>
+						Nintendo Switch Hardware Review
+					</p>
 				</div>
 				<hr/>
 				<div>
@@ -144,8 +141,8 @@ $template->drawMainOpen();
 						<img src="http://" class="attachment-post-thumbnail" alt="" />
 					</a>
 					<p id='title of post'>
-													Nintendo Switch Software Review
-												</p>
+						Nintendo Switch Software Review
+					</p>
 				</div>
 				<hr/>
 				<div>
@@ -167,8 +164,8 @@ $template->drawMainOpen();
 						<img src="http://" class="attachment-post-thumbnail" alt="" />
 					</a>
 					<p id='title of post'>
-													Nintendo Switch Software Review
-												</p>
+						Nintendo Switch Software Review
+					</p>
 				</div>
 				<hr/>
 				<div>
@@ -190,8 +187,8 @@ $template->drawMainOpen();
 						<img src="http://" class="attachment-post-thumbnail" alt="" />
 					</a>
 					<p id='title of post'>
-													Nintendo Switch Software Review
-												</p>
+						Nintendo Switch Software Review
+					</p>
 				</div>
 				<hr/>
 				<div>
