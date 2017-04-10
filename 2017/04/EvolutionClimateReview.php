e42fb2296a5cfb40c41b867e5b80a2dfe0c87454
Ryan Meyer <Ryan Meyer>
2017-04-10 15:29:37 -0700

    Managing new article template

--- 2017/04/EvolutionClimateReview.php
+++ 2017/04/EvolutionClimateReview.php
@@ -36,23 +36,23 @@ $template->drawMainOpen();
 							Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate
 							Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat Evolution Climate Killer Strat
 						</p>
-						<p id="testingtest">
+						<p class="article-paragraph">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut varius nisi. Vestibulum blandit, enim at aliquet pharetra,
 					       lacus diam vulputate tortor, sed hendrerit diam purus eget urna. Cras porttitor eleifend leo, et faucibus neque venenatis ac.
 						</p>
-						<p id="testingtest">
+						<p class="article-paragraph">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut varius nisi. Vestibulum blandit, enim at aliquet pharetra,
 					       lacus diam vulputate tortor, sed hendrerit diam purus eget urna. Cras porttitor eleifend leo, et faucibus neque venenatis ac.
 						</p>
-						<p id="testingtest">
+						<p class="article-paragraph">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut varius nisi. Vestibulum blandit, enim at aliquet pharetra,
 					       lacus diam vulputate tortor, sed hendrerit diam purus eget urna. Cras porttitor eleifend leo, et faucibus neque venenatis ac.
 						</p>
-						<p id="testingtest">
+						<p class="article-paragraph">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut varius nisi. Vestibulum blandit, enim at aliquet pharetra,
 					       lacus diam vulputate tortor, sed hendrerit diam purus eget urna. Cras porttitor eleifend leo, et faucibus neque venenatis ac.
 						</p>
-						<p id="testingtest">
+						<p class="article-paragraph">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut varius nisi. Vestibulum blandit, enim at aliquet pharetra,
 					       lacus diam vulputate tortor, sed hendrerit diam purus eget urna. Cras porttitor eleifend leo, et faucibus neque venenatis ac.
 						</p>
